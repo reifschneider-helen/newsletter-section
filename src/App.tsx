96803d9ba4daae7cdaa4cff9518eba30b85225ce
reifschneider-helen <reifschneider.helen@gmail.com>
2025-06-26 14:25:24 +0200

    Add styles and icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,37 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <div className="left-column">
+        <h1>Get the finest curated abstracts delivered weekly to yout inbox</h1>
+        <div className="benefits-text">
+        <p className="with-icon">
+          Exclusive access to new abstract images and collections
+        </p>
+        <p className="with-icon">
+          Unlock special promotions only for subscribers
+        </p>
+        <p className="with-icon">Regular doses of artistic inspiration</p>
+        </div>
+        <div className="subscribe-field">
+          <label className="sr-only" htmlFor="email-input">Email</label>
+          <input
+            id="email-input"
+            type="email"
+            placeholder="Enter your email"
+          ></input>
+          <button>Subscribe</button>
+        </div>
+        <p className="disclaimer">We only send the best! No spam.</p>
+      </div>
+      <div className="right-column">
+        <img
+          src="/assets/img/abstract.jpg"
+          width="500"
+          alt="abstract image"
+        ></img>
+      </div>
     </div>
   );
 }
