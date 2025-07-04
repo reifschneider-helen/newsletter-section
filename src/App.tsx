0c349d5db67d31738635eeb3326ccd4ffee5dab9
reifschneider-helen <reifschneider.helen@gmail.com>
2025-07-04 16:15:23 +0200

    Add more styles

--- src/App.tsx
+++ src/App.tsx
@@ -6,22 +6,29 @@ function App() {
       <div className="left-column">
         <h1>Get the finest curated abstracts delivered weekly to yout inbox</h1>
         <div className="benefits-text">
-        <p className="with-icon">
-          Exclusive access to new abstract images and collections
-        </p>
-        <p className="with-icon">
-          Unlock special promotions only for subscribers
-        </p>
-        <p className="with-icon">Regular doses of artistic inspiration</p>
+          <p className="with-icon">
+            Exclusive access to new abstract images and collections
+          </p>
+          <p className="with-icon">
+            Unlock special promotions only for subscribers
+          </p>
+          <p className="with-icon">Regular doses of artistic inspiration</p>
         </div>
         <div className="subscribe-field">
-          <label className="sr-only" htmlFor="email-input">Email</label>
-          <input
-            id="email-input"
-            type="email"
-            placeholder="Enter your email"
-          ></input>
-          <button>Subscribe</button>
+          <form className="subscribe-form" method="POST" action="/users/signup">
+            <label className="sr-only" htmlFor="email-input">
+              Email
+            </label>
+            <input
+              autoComplete="email"
+              id="email-input"
+              name="userEmail"
+              required
+              type="email"
+              placeholder="Enter your email"
+            ></input>
+            <button>Subscribe</button>
+          </form>
         </div>
         <p className="disclaimer">We only send the best! No spam.</p>
       </div>
